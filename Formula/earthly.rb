fd405dd2bd7300dc56f075563f9830c1b13765fd
griswoldthecat <griswoldthecat@users.noreply.github.com>
2022-08-19 17:20:08 +0000

    earthly 0.6.22

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -1,8 +1,8 @@
 class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
-  url "https://github.com/earthly/earthly/archive/v0.6.21.tar.gz"
-  sha256 "11839d220f0e049b3679634d397bfb0542035c9f42ab400279f46358bce4dafd"
+  url "https://github.com/earthly/earthly/archive/v0.6.22.tar.gz"
+  sha256 "efd495ec78f51d4c9e58f2d5ea8fe790c85512867607e031b2e8261c3984b746"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git", branch: "main"
 
@@ -23,7 +23,7 @@ class Earthly < Formula
     # the ldflags string will break the upstream release process.
     earthly_gitsha = "7e4f1df4c124db1644d51d312b19313217cbe478"
 
-    ldflags = "-X main.DefaultBuildkitdImage=docker.io/earthly/buildkitd:v#{version} -X main.Version=v#{version} -X main.GitSha=a24b203e9770d822c9161c7cafd83ba52b6db745 " \
+    ldflags = "-X main.DefaultBuildkitdImage=docker.io/earthly/buildkitd:v#{version} -X main.Version=v#{version} -X main.GitSha=1c8bcfbab64b9b52302ef63c897e6b989b88a794 " \
               "-X main.GitSha=#{earthly_gitsha}"
     tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork dfheredoc forceposix"
     system "go", "build",
