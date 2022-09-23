f5aff45b95428274f4e3263a26a4a1e653dd342b
Alex Couture-Beil <alex@earthly.dev>
2022-09-23 09:31:00 -0700

    earthly: use generate_completions DSL
    Signed-off-by: Alex Couture-Beil <alex@earthly.dev>

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -32,10 +32,7 @@ class Earthly < Formula
         *std_go_args,
         "./cmd/earthly"
 
-    bash_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "bash")
-    (bash_completion/"earthly").write bash_output
-    zsh_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "zsh")
-    (zsh_completion/"_earthly").write zsh_output
+    generate_completions_from_executable(bin/"earthly", "bootstrap", "--source", shells: [:bash, :zsh])
   end
 
   test do
