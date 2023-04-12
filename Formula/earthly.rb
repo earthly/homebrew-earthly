207f92cad5aa1cb4a7aa319d7db74168816f6c9f
littleredcorvette <littleredcorvette@users.noreply.github.com>
2023-04-12 20:36:12 +0000

    earthly 0.7.4

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -2,8 +2,8 @@ class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
   url "https://github.com/earthly/earthly.git",
-      tag:      "v0.7.2",
-      revision: "e6fed708700425fc2616bd1e7a0f202d414f5f23"
+      tag:      "v0.7.4",
+      revision: "a1c46ca2b75f21a4948fd53b5464de5a2bd90372"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git", branch: "main"
 
